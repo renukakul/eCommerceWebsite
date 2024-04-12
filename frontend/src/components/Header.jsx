f7374d918e4f5f441fb937536c724a24b2eb1f1a
Renuka Kulkarni <renukakulkarni@Rahuls-MacBook-Pro.local>
2024-04-11 20:59:20 -0400

    Cutom Bootstrap Style

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Navbar, Nav , Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import logo from '../assets/logo.png'
 
 
 const Header = () => {
@@ -8,7 +9,9 @@ const Header = () => {
     <header>
         <Navbar bg='dark' variant='dark' expand ="lg" collapseOnSelect>
           <Container>
-            <Navbar.Brand href = '/'>Proshop</Navbar.Brand>
+          <Navbar.Brand href = '/'>
+              <img src={logo} alt ='proshop' />
+              Proshop</Navbar.Brand>
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className='ms-auto'>
