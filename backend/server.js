f727abe5af7633d019563370060242aaf917c4be
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2024-04-12 14:30:04 -0400

    environ variable added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+const port = process.env.PORT || 5000;
+
+
 import products from './data/products.js';
-const port = 5000;
+
 
 const app = express();
 
