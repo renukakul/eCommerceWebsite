a626f380e88109b7bd62a79e45ff2bd2f657d644
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2024-04-12 16:40:49 -0400

    redux store, apislice, product slice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,33 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import {
-  createBrowserRouter, 
+  createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider } from 'react-router-dom'
+  RouterProvider,
+} from "react-router-dom";
 // import './index.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/styles/index.css';
-import './assets/styles/bootstrap.custom.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-
+import "./assets/styles/index.css";
+import "./assets/styles/bootstrap.custom.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import store from "./store";
+import { Provider } from "react-redux";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route index={true} path='/' element={<HomeScreen />} />
-      <Route path='/product/:id' element={<ProductScreen /> } />
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
-)
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-   <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,31 +1,33 @@
 import { Row, Col } from 'react-bootstrap';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
+import { useGetProductsQuery } from '../slices/productSlice';
 import Product from '../components/Product';
 // import products from '../products-and-images/products'
 
-import axios from 'axios'
+// import axios from 'axios'
 
 
 const HomeScreen = () => {
-  const[products, setProducts] = useState([]);
+  const { data: products, isLoading, error } = useGetProductsQuery();
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const{ data } = await axios.get('/api/products');
-      setProducts(data);
-    };
-    fetchProducts();
-  },[])
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+       {isLoading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error?.data.message || error.error}</div>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
