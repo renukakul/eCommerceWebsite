a080cac035d51aeea85b1989b862556126fc84c0
Renuka Kulkarni <renukakulkarni@Rahuls-MacBook-Pro.local>
2024-04-11 20:40:47 -0400

    added Header and Footer Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Container } from 'react-bootstrap';
+import Header from './components/Header'
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <p>welcome to Proshop</p>
+    <>
+      <Header />
+        <main className='py-3'>
+          <Container>
+            <h1> Welcome To ProShop </h1>
+          </Container>
+          <Footer />
+        </main>
+    </>
   )
 }
 
