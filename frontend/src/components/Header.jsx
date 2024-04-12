5e7dd9cc39762e4513db82302377ce5ef2908279
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2024-04-12 19:24:38 -0400

    Item count in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {LinkContainer } from 'react-router-bootstrap';
-import { Navbar, Nav , Container } from 'react-bootstrap';
+import { Navbar, Nav , Container, Badge } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
 import logo from '../assets/logo.png'
 
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
   return (
     <header>
         <Navbar bg='dark' variant='dark' expand ="lg" collapseOnSelect>
@@ -20,7 +22,11 @@ const Header = () => {
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className='ms-auto'>
               <LinkContainer to='/cart'>
-                  <Nav.Link><FaShoppingCart /> Cart </Nav.Link>
+                  <Nav.Link><FaShoppingCart /> Cart  {cartItems.length > 0 && (
+                      <Badge pill bg='success' style={{ marginLeft: '5px' }}>
+                          {cartItems.reduce((a, c) => a + Number(c.qty), 0)}
+                    </Badge>
+                  )}</Nav.Link>
                 </LinkContainer>
                 <LinkContainer to='/login'>
                   <Nav.Link ><FaUser /> Sign Up</Nav.Link>
