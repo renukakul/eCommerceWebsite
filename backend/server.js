d02ce661b6ba1cd7ac01054c8c30666da2eadbc4
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2024-04-12 15:45:06 -0400

    added route to fetch data from database

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 
 import connectDB from './config/db.js'
-import products from './data/products.js';
+// import products from './data/products.js';
+import productRoutes from './routes/productRoutes.js';
 
 connectDB();
 const app = express();
@@ -13,14 +14,7 @@ app.get('/', (req, res) => {
     res.send("API is Running...")
 });
 
-app.get('/api/products', (req, res) =>{
-    res.json(products);
-})
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product)
-})
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`));
 
