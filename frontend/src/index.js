0b854c1398594c4306dfb6b94254b4fce01f56a1
Renuka Kulkarni <renukakulkarni@Rahuls-MacBook-Pro.local>
2024-04-14 13:22:26 -0400

    Add and Remove from the cart

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 import store from "./store";
 import { Provider } from "react-redux";
 
@@ -22,6 +23,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path='/cart' element={<CartScreen />} />
     </Route>
   )
 );
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,25 +1,44 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { updateCart } from '../utils/cartUtils';
+import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
 
-const initialState = localStorage.getItem('cart')
-  ? JSON.parse(localStorage.getItem('cart'))
+const initialState = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
   : { cartItems: [] };
 
-
 const cartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState,
   reducers: {
     addToCart: (state, action) => {
-      // The item to add to the cart 
+      // The item to add to the cart
       const item = action.payload;
 
-    // Update the cart state using the updateCart function
-    return updateCart(state, item);
+      // Check if the item is already in the cart
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+
+      if (existItem) {
+        // If exists, update quantity
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
+      } else {
+        // If not exists, add new item to cartItems
+        state.cartItems = [...state.cartItems, item];
+      }
+
+      // Update the prices and save to storage
+      return updateCart(state, item);
+    },
+    removeFromCart: (state, action) => {
+      // Filter out the item to remove from the cart
+      state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
+
+      // Update the prices and save to storage
+      return updateCart(state);
     },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
