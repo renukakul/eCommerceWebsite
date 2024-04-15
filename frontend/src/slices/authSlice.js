9c3917ad4d2acc7fab9ee086f40ecfab5496347c
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2024-04-15 16:46:12 -0400

    User Logout

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -21,6 +21,6 @@ const authSlice = createSlice({
   },
 });
 
-export const { setCredentials} = authSlice.actions;
+export const { setCredentials, logout} = authSlice.actions;
 
 export default authSlice.reducer;
