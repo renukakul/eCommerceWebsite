2ce88c0ea98217a8045581ec30503c296d7132c6
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2024-04-15 17:36:12 -0400

    Shipping Screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,6 +17,7 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreens";
 import RegisterScreen from "./screens/RegisterScreen";
+import ShippingScreen from "./screens/ShippingScreen";
 
 import store from "./store";
 import { Provider } from "react-redux";
@@ -30,6 +31,7 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
+      <Route path="/shipping" element={<ShippingScreen />} />
     </Route>
   )
 );
