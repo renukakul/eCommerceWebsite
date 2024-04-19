56e0ce4eba0662874240bf7e46bc72f6c2fbd366
Renuka Kulkarni <renuka.kulkarni7087@gmail.com>
2024-04-19 18:58:17 -0400

    prepared for production

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,14 @@ app.get('/api/config/paypal', (req, res) =>
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+if (process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  //any route that is not api will be redirected to index.html
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+}
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
